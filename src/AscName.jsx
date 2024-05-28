fa1bed834c6a13efd5448b9c1d02f2effe5b24a3
Wojciech Bujna <51890212+Bujnygrzywes@users.noreply.github.com>
2024-05-28 14:56:17 +0200

    added propTypes to each component

--- src/AscName.jsx
+++ src/AscName.jsx
@@ -1,14 +1,24 @@
-export  default  function  AscName({ fruits })
-{
-    const sortedFruits  =  [...fruits].sort((a, b) => a.name.localeCompare(b.name));
-    const reversedFruits = sortedFruits.reverse();
+import PropTypes from "prop-types";
+
+export default function AscName({ fruits }) {
+  const sortedFruits = [...fruits].sort((a, b) => a.name.localeCompare(b.name));
+  const reversedFruits = sortedFruits.reverse();
   return (
     <div className={"container"}>
-        {reversedFruits.map((fruit) => (
-          <div key={fruit.name}>
-            {fruit.name} - {fruit.calories} calories
-          </div>
-        ))}
+      {reversedFruits.map((fruit) => (
+        <div key={fruit.name}>
+          {fruit.name} - {fruit.calories} calories
+        </div>
+      ))}
     </div>
   );
 }
+
+AscName.propTypes = {
+  fruits: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      calories: PropTypes.number,
+    })
+  ),
+};
